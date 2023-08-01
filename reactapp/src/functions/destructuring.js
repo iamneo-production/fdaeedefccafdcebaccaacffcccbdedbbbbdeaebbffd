4516b5d3df3d8e8e3c0e2393784690eb065f7f15
Iamneo <techadmin@iamneo.ai>
2023-08-01 13:44:50 +0000

    Backup Commit

--- reactapp/src/functions/destructuring.js
+++ reactapp/src/functions/destructuring.js
@@ -7,5 +7,4 @@ function getTopThree(studentMarks = []) {
   const [firstMark = null, secondMark = null, thirdMark = null] = studentMarks;
   return [firstMark, secondMark, thirdMark].sort();
 }
-
 export { getUserIpDetails,getTopThree};
